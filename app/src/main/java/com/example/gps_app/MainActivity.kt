043bd0d8b7d4689ecfcb6c56a53508bde4191d83
Joel Mercado <joel.mercado@ine.mx>
2021-07-28 16:24:46 -0500

    Primer commit para el proyecto GPS

---
+++ app/src/main/java/com/example/gps_app/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.example.gps_app
+
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity"
+
+    // Storage Permissions
+    private val REQUEST_LOCATION = 1
+    private val PERMISSIONS_LOCATION = arrayOf<String>(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_LOCATION -> {
+
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+
+                    Log.i(TAG, "Permission has been denied by user")
+                } else {
+                    Log.i(TAG, "Permission has been granted by user")
+                }
+            }
+        }
+    }
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    private fun verifyLocationPermissions(activity: Activity?) {
+        // Check if we have write permission
+        val permission: Int = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                this,
+                PERMISSIONS_LOCATION,
+                REQUEST_LOCATION
+            )
+        }
+    }
+
+
+    // inside a basic activity
+    private var locationManager : LocationManager? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        verifyLocationPermissions(this)
+
+//        setSupportActionBar(toolbar)
+
+        // Create persistent LocationManager reference
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
+
+        fab.setOnClickListener {
+            try {
+                // Request location updates
+                    Log.d(TAG,"setOnClickListener")
+                locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener)
+            } catch(ex: SecurityException) {
+                ex.printStackTrace()
+                Log.d(TAG, "Security Exception, no location available")
+            }
+        }
+
+    }
+
+    //define the listener
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            Log.d(TAG, "onLocationChanged()")
+            posc_lat.text = ("lat: " + location.latitude)
+            posc_long.text = ("lon:" + location.longitude)
+        }
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
+            Log.d(TAG, "onStatusChanged()")
+
+        }
+        override fun onProviderEnabled(provider: String) {
+            Log.d(TAG, "onProviderEnabled()")
+
+        }
+        override fun onProviderDisabled(provider: String) {
+            Log.d(TAG, "onProviderDisabled()")
+
+        }
+    }
+}
